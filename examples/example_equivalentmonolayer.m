d14801b7a724e6ed8922959fee47b51113fa9c40
tgoossens <thomas@arch-thomas.localdomain>
2021-04-11 14:41:26 +0200

    More documentation example equivalent monolayer

--- examples/example_equivalentmonolayer.m
+++ examples/example_equivalentmonolayer.m
@@ -1,4 +1,14 @@
+%% Comparison of tiny Fabry-Pérot filter and equivalent tiny monolayer
+% This example demonstrated how the transmittance of a tiny dielectric Fabry-Pérot filter
+% differs from equivalent monolayer response.
+%
+% It shows that a well chosen equivalent monolayer approximates the response of a Fabry-pérot well. That is, given that the Fabry-Pérot is nicely approximated by a Lorentzian.
+%
+%
+% Copyright Thomas Goossens
+
 clear; close all;
+
 
 addpath('/home/thomas/Documents/tinyfilters/research/wavepacket')
 
@@ -30,11 +40,11 @@ neff=nl*sqrt(1/(1-nl/nh+nl^2/nh^2));
     
 %% Choose simulation options
 
-polarisation = 's';
+polarisation = 'unpolarized';
 
 accuracy = 7;
 wavelengths=linspace(0.73,0.85,300); % µm
-angles = [0 5 10 15 20 ]; 
+angles = [0 10 15 20 ]; 
 
 %% Run simulation for each angle
 for a=1:numel(angles)
@@ -46,23 +56,16 @@ for a=1:numel(angles)
     Tclassic(:,a)=classictransmittance(filter,angles(a),wavelengths,polarisation);
     L=fwhm(wavelengths,Tclassic(:,1))/targetcwl;
     
-    % Isimplicitly also unpolarized
-    [Tmono]=tinytransmittance_mono(targetcwl,L,neff,width,nsub,angles(a),wavelengths,polarisation,accuracy);
-    Tm(:,a)=Tmono;
+
+    Tmono(:,a)=tinytransmittance_mono(targetcwl,L,neff,width,nsub,angles(a),wavelengths,polarisation,accuracy);
     
-    
-    % Unpolarized 
-    [Ttiny_s]=tinytransmittance(filter,angles(a),wavelengths,polarisation,accuracy);
-    [Ttiny_p]=tinytransmittance(filter,angles(a),wavelengths,'p',accuracy);
-    T(:,a)=0.5*(Ttiny_s+Ttiny_p);
-    %    T(:,a)=Ttiny_s;
+    Ttiny(:,a)=tinytransmittance(filter,angles(a),wavelengths,polarisation,accuracy);
+
     
 end
 
 
 %% Plot transmittance
-% There there is a drop in transmittance and an increase in FWHM
-% 
 
 cmap = hot;
 s=size(cmap,1);
@@ -75,29 +78,25 @@ color{5}=cmap(round(0.66*s),:)
 
 figure(1);clf;  hold on;
 for a=1:numel(angles)
-    htiny(a)=plot(wavelengths,T(:,a),'color',color{a},'linewidth',2)
-    hmono(a)=plot(wavelengths,Tm(:,a)/max(Tm(:,1))*max(T(:,1)),'color','m','linewidth',2)
-    hclassic=plot(wavelengths,Tclassic(:,a),':','color',color{a},'linewidth',1.5)
+    subplot(2,2,a); hold on;
+    htiny=plot(wavelengths,Ttiny(:,a),'color',color{2},'linewidth',2)
+    hmono=plot(wavelengths,Tmono(:,a),':','color',color{4},'linewidth',2)
+    hclassic=plot(wavelengths,Tclassic(:,a),':','color','k','linewidth',1.5)
+    ylabel('Transmittance')
+    xlabel('Wavelength (µm)')
 end
 
 
 
 
-%% Labeling
-
-text(0.73,0.6092,sprintf('Transmittance for\ninfinitely wide filter'))
-line( [0.7617   0.7790],[ 0.5684    0.4888],'color','k')
-
-text(0.73,0.2092,sprintf('Transmittance for\ntiny filter'))
-line( [ 0.7608    0.7729],[  0.1969    0.1255],'color','k')
-
-legend([htiny],'0^\circ','5^\circ','10^\circ','15^\circ','20^\circ')
+legh=legend([htiny hmono hclassic],'Tiny Fabry-Pérot','Equivelent monolayer','Infinite filter')
+legh.Orientation='horizontal';
+legh.Position=[0.1511 0.9469 0.7625 0.0429];
 
 
 
-ylabel('Transmittance')
-xlabel('Wavelength (µm)')
-title('Tiny vs. infinite transmittance')
+
+
 box on
 
 
