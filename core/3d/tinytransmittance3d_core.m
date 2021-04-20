c9ef72287385599205bd21b638a72e5b00d96950
tgoossens <thomas@arch-thomas.localdomain>
2021-04-20 22:58:53 +0200

    Started work on new interface where an arbitrary incident wavepacket can be provided to the simulator

--- core/3d/tinytransmittance3d_core.m
+++ core/3d/tinytransmittance3d_core.m
@@ -1,35 +1,16 @@
 
-function [T,Phi_t,Phi_in] = tinytransmittance3dfocus_core(filter,coneangle_deg,cra_deg,azimuth_deg,wavelengths,polarization,pixelkernel,accuracy);
-%  TINYTRANSMITTANCE3D_MONO  Simulate tiny filter transmittance
-%   [T] = TINYTRANSMITTANCE3D_MONO(filter,angledeg,wavelengths,polarization,accuracy);
-%
-%   Inputs
-%    - filter : Struct containing the tiny filter design (See also TINYFILTER)
-%    - angledeg:  Incidence angle in degrees
-%    - wavelengths (Wx1): Wavelengths (same units as filter.width of filter)
-%    - polarization ('s' or 'p')
-%    - accuracy: 2^floor(accuracy) subdivision of the spatial frequency domain.
-%   Outputs
-%    - T (Wx1):  Transmittance of the filter
-%    - Phi_T (Wx1):  Transmitted flux [W]
-%    - Phi_T (Wx1):  Incident flux [W]
-%
-%
-%  See also TINYFILTER
-%  Copyright Thomas Goossens
-%  http://github.com/tgoossens
-
+function [T,Phi_t,Phi_in] = tinytransmittance3d_core(filter,incident_wavepacket,wavelengths,polarization,accuracy,pixelkernel);
 
 
     %% TODO 
-% if(or(polarization=='unpolarized',polarization=='unpolarised'))
-%     [T_s,Phi_t_s,Phi_in_s] = tinytransmittance3dfocus_rotational_mono(central_wavelength,normalized_fwhm,effective_index,width,nsub,coneangle_deg,cra_deg,alpha_deg,wavelengths,'s',pixelkernel,accuracy);
-%     [T_p,Phi_t_p,Phi_in_p] =  tinytransmittance3dfocus_rotational_mono(central_wavelength,normalized_fwhm,effective_index,width,nsub,coneangle_deg,cra_deg,alpha_deg,wavelengths,'p',pixelkernel,accuracy);
-%     T =  0.5*(T_s+T_p);
-%     Phi_t =  0.5*(Phi_t_s+Phi_t_p);
-%     Phi_in =  0.5*(Phi_t_s+Phi_t_p);
-%     return;
-% end
+    % if(or(polarization=='unpolarized',polarization=='unpolarised'))
+    %     [T_s,Phi_t_s,Phi_in_s] = tinytransmittance3dfocus_rotational_mono(central_wavelength,normalized_fwhm,effective_index,width,nsub,coneangle_deg,cra_deg,alpha_deg,wavelengths,'s',pixelkernel,accuracy);
+    %     [T_p,Phi_t_p,Phi_in_p] =  tinytransmittance3dfocus_rotational_mono(central_wavelength,normalized_fwhm,effective_index,width,nsub,coneangle_deg,cra_deg,alpha_deg,wavelengths,'p',pixelkernel,accuracy);
+    %     T =  0.5*(T_s+T_p);
+    %     Phi_t =  0.5*(Phi_t_s+Phi_t_p);
+    %     Phi_in =  0.5*(Phi_t_s+Phi_t_p);
+    %     return;
+    % end
 
 
 wl=reshape(wavelengths,[1 1 numel(wavelengths)]); 
@@ -44,7 +25,6 @@ nu_x = linspace(-1/wl(1), 1/wl(1),2^floor(accuracy))';
 nu_y=nu_x';
 nu = sqrt(nu_x.^2+nu_y.^2);
 %nu = linspace(-10/wl(1), 10/wl(1),2^floor(accuracy))';
-
 
 
 %% Definitions and helper ufcntions
@@ -79,18 +59,13 @@ eta_sub=eta(1);
 eta = admittance(1,wl,nu,polarization);
 eta_in=eta(1);
 
-
-%% Incident field
-wavepacket_in=wavepacket_3dfocus(coneangle_deg,cra_deg,azimuth_deg,width);
-
-
 %% Simulate
 
 for j=1:numel(wl)
     %%%%%%%%%% WAVE AMPLITUDES %%%%%%%%%%%%
     % Incident wwave
 
-    Ain(:,:,j) = wavepacket_in(nu_x,nu_y,wl(j));
+    Ain(:,:,j) = incident_wavepacket(nu_x,nu_y,wl(j));
     
     % Useful integration domain;. This conditions corresponds to ignore incidence angles larger than 90 degres.
     domain = abs(nu).*wl(j) <=1;
---
+++ core/wavepackets/wavepacket3d_focus.m
@@ -0,0 +1,26 @@
+
+function wavepacket =  wavepacket3d_focus(coneangle_deg,cra_deg,azimuth_deg,filterwidth) 
+     
+    wavepacket = @field;
+    function wavepacket_in = field(nu_x,nu_y,wavelength)    
+        circ = @(u,radius) double(abs(u)<radius);
+
+        zi=1; % it doesn't matter for the pupil function (it is normalized out)
+              % however for comparison with the equations in article i keep it in
+
+        di=zi*tand(cra_deg);
+        lensradius=zi*tand(coneangle_deg);
+        P0 = @(x,y) circ(sqrt(x.^2+y.^2),lensradius);
+        P = @(x,y) P0(x+di*cosd(azimuth_deg),y+di*sind(azimuth_deg));
+        filteraperture = filterwidth^2 *sinca(pi*filterwidth*nu_x).*sinca(pi*filterwidth*nu_y);
+        pupilfunction= (P(-wavelength.*zi.*nu_x,-wavelength.*zi.*nu_y));
+        wavepacket_in = conv2fft(pupilfunction,filteraperture,'same');  
+    end
+    
+    function f = sinca(x)
+    % Modified sinc function because matlab sinc function already includes the factor pi.
+    % This makes notation consistent with definitions in the publications.
+        f=sinc(x/pi);
+    end
+    
+end
